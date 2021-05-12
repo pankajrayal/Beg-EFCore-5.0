d3a673399f4de61956846ad1e5f0f5765b55bcba
Pankaj Rayal <https://github.dxc.com/>
2021-05-12 20:54:27 +0530

    04-Database Context

---
+++ CH03/EFCore5WebApp.DAL/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EFCore5WebApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore5WebApp.DAL {
+    public class AppDbContext : DbContext {
+        public AppDbContext() : base() { }
+        public AppDbContext(DbContextOptions options) : base(options) {}
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<LookUp> LookUps { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+    }
+}
