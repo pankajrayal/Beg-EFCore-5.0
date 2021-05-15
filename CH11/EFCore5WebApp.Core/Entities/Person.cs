f864ece0d5310497815207069b4ac46d78569ab3
Pankaj Rayal <https://github.dxc.com/>
2021-05-15 17:17:55 +0530

    11-Navigation Properties

---
+++ CH11/EFCore5WebApp.Core/Entities/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCore5WebApp.Core.Entities {
+    [Table("Persons", Schema = "dbo")]
+    public class Person {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string LastName { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+
+        public List<Address> Addresses { get; set; } = new List<Address>();
+        [NotMapped]
+        public string FullName => $"{FirstName} {LastName}";
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public List<Person> Parets { get; set; } = new List<Person>();
+        public List<Person> Children { get; set; } = new List<Person>();
+    }
+}
---
+++ CH11/EFCore5WebApp.DAL/Migrations/20210515112217_PersonParentChildren.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore5WebApp.DAL.Migrations {
+    public partial class PersonParentChildren : Migration {
+        protected override void Up(MigrationBuilder migrationBuilder) {
+            migrationBuilder.CreateTable(
+                name: "PersonPerson",
+                schema: "dbo",
+                columns: table => new {
+                    ChildrenId = table.Column<int>(type: "int", nullable: false),
+                    ParetsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table => {
+                    table.PrimaryKey("PK_PersonPerson", x => new { x.ChildrenId, x.ParetsId });
+                    table.ForeignKey(
+                        name: "FK_PersonPerson_Persons_ChildrenId",
+                        column: x => x.ChildrenId,
+                        principalSchema: "dbo",
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonPerson_Persons_ParetsId",
+                        column: x => x.ParetsId,
+                        principalSchema: "dbo",
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2021, 5, 15, 16, 52, 16, 453, DateTimeKind.Local).AddTicks(6080));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedOn",
+                value: new DateTime(2021, 5, 15, 16, 52, 16, 454, DateTimeKind.Local).AddTicks(2231));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonPerson_ParetsId",
+                schema: "dbo",
+                table: "PersonPerson",
+                column: "ParetsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder) {
+            migrationBuilder.DropTable(
+                name: "PersonPerson",
+                schema: "dbo");
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2021, 5, 15, 16, 3, 19, 523, DateTimeKind.Local).AddTicks(4505));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedOn",
+                value: new DateTime(2021, 5, 15, 16, 3, 19, 524, DateTimeKind.Local).AddTicks(6316));
+        }
+    }
+}
