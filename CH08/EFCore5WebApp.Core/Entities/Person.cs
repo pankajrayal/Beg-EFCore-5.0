1a20d5e2af15aeb6d5de2125ed13174725021189
Pankaj Rayal <https://github.dxc.com/>
2021-05-15 15:44:21 +0530

    08-Inserting Data

---
+++ CH08/EFCore5WebApp.Core/Entities/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCore5WebApp.Core.Entities {
+    [Table("Persons", Schema = "dbo")]
+    public class Person {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string LastName { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+        public List<Address> Addresses { get; set; } = new List<Address>();
+    }
+}
