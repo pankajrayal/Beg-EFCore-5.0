9f6f7074d7c604ee1758eb1d252df9a2a857b072
Pankaj Rayal <https://github.dxc.com/>
2021-05-15 16:06:48 +0530

    09-Updating Data

---
+++ CH09/EFCore5WebApp.Core/Entities/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCore5WebApp.Core.Entities {
+    [Table("Persons", Schema = "dbo")]
+    public class Person {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string LastName { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public List<Address> Addresses { get; set; } = new List<Address>();
+    }
+}
