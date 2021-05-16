2d8be64e8aec8be85b320eb91ed21a765b3d23a7
Pankaj Rayal <https://github.dxc.com/>
2021-05-16 20:37:40 +0530

    18-Updating Data on the Web

---
+++ CH18/EFCore5WebApp.DAL/Migrations/20210516083851_AddedAddressValidation.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore5WebApp.DAL.Migrations {
+    public partial class AddedAddressValidation : Migration {
+        protected override void Up(MigrationBuilder migrationBuilder) {
+            migrationBuilder.AlterColumn<string>(
+                name: "ZipCode",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "State",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AddressLine1",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "PersonPerson",
+                schema: "dbo",
+                columns: table => new {
+                    ChildrenId = table.Column<int>(type: "int", nullable: false),
+                    ParentsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table => {
+                    table.PrimaryKey("PK_PersonPerson", x => new { x.ChildrenId, x.ParentsId });
+                    table.ForeignKey(
+                        name: "FK_PersonPerson_Persons_ChildrenId",
+                        column: x => x.ChildrenId,
+                        principalSchema: "dbo",
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonPerson_Persons_ParentsId",
+                        column: x => x.ParentsId,
+                        principalSchema: "dbo",
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2021, 5, 16, 14, 8, 51, 130, DateTimeKind.Local).AddTicks(978));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedOn",
+                value: new DateTime(2021, 5, 16, 14, 8, 51, 130, DateTimeKind.Local).AddTicks(8546));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonPerson_ParentsId",
+                schema: "dbo",
+                table: "PersonPerson",
+                column: "ParentsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder) {
+            migrationBuilder.DropTable(
+                name: "PersonPerson",
+                schema: "dbo");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ZipCode",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "State",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AddressLine1",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2021, 5, 16, 11, 44, 54, 563, DateTimeKind.Local).AddTicks(1824));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedOn",
+                value: new DateTime(2021, 5, 16, 11, 44, 54, 564, DateTimeKind.Local).AddTicks(3103));
+        }
+    }
+}
