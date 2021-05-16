de1a3cf13cac76d4b16a412f598a959c2c57bc4b
Pankaj Rayal <https://github.dxc.com/>
2021-05-16 12:39:39 +0530

    15-Authentication on the Web

---
+++ CH15/EFCore5WebApp.Core/Entities/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCore5WebApp.Core.Entities {
+    [Table("Persons", Schema = "dbo")]
+    public class Person {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string LastName { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+
+        public List<Address> Addresses { get; set; }
+        [NotMapped]
+        public string FullName => $"{FirstName} {LastName}";
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int Age { get; set; }
+    }
+}
