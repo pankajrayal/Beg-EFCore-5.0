25e2d51688580e9d2a0fdd4d33f95b25819c882d
Pankaj Rayal <https://github.dxc.com/>
2021-05-16 09:38:44 +0530

    14-Migrations

---
+++ CH14/EFCore5WebApp.DAL/Migrations/20210515162634_CommitPendingChanges.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore5WebApp.DAL.Migrations
+{
+    public partial class CommitPendingChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2021, 5, 15, 21, 56, 33, 624, DateTimeKind.Local).AddTicks(8966));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedOn",
+                value: new DateTime(2021, 5, 15, 21, 56, 33, 625, DateTimeKind.Local).AddTicks(6714));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2021, 5, 15, 20, 32, 16, 671, DateTimeKind.Local).AddTicks(2952));
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedOn",
+                value: new DateTime(2021, 5, 15, 20, 32, 16, 672, DateTimeKind.Local).AddTicks(7920));
+        }
+    }
+}
